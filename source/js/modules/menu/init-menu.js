79a20c0f60e85b110e62c3d289598caa59f1c879
dmitry.buyan <buyandl@mail.ru>
2022-03-28 22:38:55 +0400

    правка багов

--- source/js/modules/menu/init-menu.js
+++ source/js/modules/menu/init-menu.js
@@ -1,23 +1,34 @@
-import {Menu} from './menu';
+const openMenu = () => {
+  const modal = document.querySelector('[data-modal]');
 
-let menu;
+  window.addEventListener('click', (evt) => {
+    const target = evt.target;
 
-const settings = {
-  'default': {
-    preventDefault: true,
-    stopPlay: true,
-    lockFocus: true,
-    startFocus: true,
-    focusBack: true,
-    eventTimeout: 400,
-    openCallback: false,
-    closeCallback: false,
-  },
+    if (target.closest('[data-open-menu]')) {
+      document.querySelector('html').classList.add('scroll-lock');
+      document.querySelector('[data-menu]').classList.add('is-active');
+      modal.classList.add('is-active');
+    }
+  });
+};
+
+const closeMenu = () => {
+  const modal = document.querySelector('[data-modal]');
+
+  window.addEventListener('click', (evt) => {
+    const target = evt.target;
+
+    if (target.closest('[data-close-menu]') || target.closest('[data-modal]')) {
+      document.querySelector('html').classList.remove('scroll-lock');
+      document.querySelector('[data-menu]').classList.remove('is-active');
+      modal.classList.remove('is-active');
+    }
+  });
 };
 
 const initMenu = () => {
-  menu = new Menu(settings);
-  window.menu = menu;
+  openMenu();
+  closeMenu();
 };
 
-export {menu, initMenu};
+export {initMenu};
